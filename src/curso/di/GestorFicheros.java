1b31aa896290da6dfe2331bf667fab0f4ecdf6af
Andrés P <spelujo@gmail.com>
2025-10-07 13:37:10 +0200

    Contador de caracteres

--- src/curso/di/GestorFicheros.java
+++ src/curso/di/GestorFicheros.java
@@ -3,43 +3,91 @@ package curso.di;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GestorFicheros {
 	private final String PATH;
 	private String textoCompleto;
 	private File fichero = null;
-	private FileReader reader = null;
-	private BufferedReader buffered = null;
-	private FileWriter writer = null;
+	private FileReader lector = null;
+	private BufferedReader reader = null;
 	
     public GestorFicheros() {
-    	PATH = "src/curso/di/resources/texto.txt";
+    	PATH = "src/curso/di/resources/paco.txt";
     	textoCompleto = new String("");
-    	fichero = null;
+    	fichero = new File(PATH);
+    	lector = null;
     	reader = null;
-    	buffered = null;
-    	writer = null;
     }
     
     public String getTextoFichero() {
     	// Leer fichero
-    	// Retorna un String con el texto del fichero
-    	String texto;
-    	
-    	fichero = new File(PATH);
-    	
-    	try (BufferedReader reader = new FileReader(fichero)) {
+    	// Retorna un String con el texto del fichero    	
+    	try {
+    		lector = new FileReader(fichero);
+    		reader = new BufferedReader(lector);
+    		
     		String line;
     		
     		while ((line = reader.readLine()) != null) {
-    			texto += line;
+    			textoCompleto += line + "\n";
     		}
-    	} catch (IOException e) {
-    		throw new RuntimeException(e);
-    	}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		System.out.println(e.getMessage());
+    	} finally {
+			try {
+				if (null != lector) {
+					lector.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
     	
-    	return texto;
+    	return textoCompleto;
     }
+    
+    public Map<String, Integer> contarTipoChar(String texto) {
+    	Map<String, Integer> mapa = new HashMap<String, Integer>();
+    	
+    	int vocales = 0;
+		int consonantes = 0;
+		int especiales = 0;
+		int resto = 0;
+		int numeros = 0;
+		
+		texto = texto.toLowerCase();
+		for (int i = 0; i < texto.length(); i++) {
+			char charActual = texto.charAt(i);
+			 
+			 if (charActual == 'a' || charActual == 'e' || charActual == 'i' || charActual == 'o' || charActual == 'u')
+				 vocales++;
+			 else if (charActual >= 'a' && charActual <= 'z')
+				 consonantes++;
+			 else if (charActual >= '0' && charActual <= '9')
+				 numeros++;
+			 else if(charActual == ';')
+				 especiales++;
+			 else
+				resto++;
+		}
+			
+		int total = vocales + consonantes + especiales + resto + numeros;
+		
+		mapa.put("Vocales", vocales);
+		mapa.put("Consonantes", consonantes);
+		mapa.put("Especiales", especiales);
+		mapa.put("Resto", resto);
+		mapa.put("Numeros", numeros);
+		mapa.put("Total", total);
+		
+		return mapa;
+    }
+    
+    
+    public void setTextFichero() {
+		//
+	}
 }
